87af8de82cc0a971ba7452297d0ee2cd551351a0
sangeon22 <tkddjsdl33@hanmail.net>
2022-04-12 23:19:09 +0900

    DNS. host, DNS 매핑, 웹서버연동 이론 및 실습

--- src/main/java/com/example/dns/dnsapp/DnsappApplication.java
+++ src/main/java/com/example/dns/dnsapp/DnsappApplication.java
@@ -2,10 +2,20 @@ package com.example.dns.dnsapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DnsappApplication {
 
+	@RestController
+	class HelloController{
+		@GetMapping("/hello")
+		public String hello(){
+			return "long time no see";
+		}
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DnsappApplication.class, args);
 	}
